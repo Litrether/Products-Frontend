014b75d7c0795c28e8996be2d25be483dc52c404
Litrether <anakinsanakins@gmail.com>
2021-07-29 17:02:06 +0300

    add headers

--- ProductFront/src/app/content/layout/header/header.component.ts
+++ ProductFront/src/app/content/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  profileStatus: string;
+
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.isAuthenticated() ? 'logout' : 'login';
+  }
+
+  logout(){
+    this.authService.logout();
   }
 
 }
