17170f84fb69fa46580dab9668a2749156d96556
HoneyBadger HoneyBadger <anakinsanakins@outlook.com>
2021-08-02 23:00:50 +0300

    redesign card, add roles

--- ProductFront/src/app/content/pages/products-page/products-page.component.ts
+++ ProductFront/src/app/content/pages/products-page/products-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/core/account/account.service';
 import { IProduct, IFoundProduct } from 'src/app/core/interfaces/products-interfaces';
 import { ProductApiService } from 'src/app/core/services/api-services/product-api.service';
 
@@ -12,6 +13,8 @@ import { ProductApiService } from 'src/app/core/services/api-services/product-ap
 export class ProductsPageComponent implements OnInit {
 
   products$: Observable<IFoundProduct[]>;
+  products: IFoundProduct[];
+  totalAmount: Number;
   currCurrency: string = 'USD';
 
   public params = {
@@ -28,15 +31,19 @@ export class ProductsPageComponent implements OnInit {
   }
 
   constructor(private router: Router,
-    private productService: ProductApiService) { 
+    private productService: ProductApiService,
+    public accountService: AccountService) { 
     }
 
   ngOnInit(): void {
-      this.products$ = this.productService.GetAllProducts(this.params);
-  }
+      this.productService.GetAllProducts(this.params).subscribe((data: any) =>{
+        this.totalAmount = data.totalAmount;
+        this.products = data.products;
+      }) 
+    }
 
   addItem() {
-    const log = this.router.navigate(['/user', 'productdetail'])
+    const log = this.router.navigate(['/user', 'productDetail'])
   }
 
   search(){
@@ -44,12 +51,12 @@ export class ProductsPageComponent implements OnInit {
     this.products$ = this.productService.GetAllProducts(this.params);
   }
 
-  ChooseCategory(category:string){
+  changeCategory(category:string){
     this.params.categories = category;
       this.products$ = this.productService.GetAllProducts(this.params);
   }
 
-  ChangeCurrency(){
+  changeCurrency(){
     this.params.currency = (<HTMLInputElement>(document.getElementById('currency-select'))).value;
     this.products$ = this.productService.GetAllProducts(this.params);
   }
